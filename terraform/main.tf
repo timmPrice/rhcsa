41ab2de679c6a0a15a37283d3abd51b59b74e2aa
timmPrice <timmPrice6@gmail.com>
2026-05-17 02:40:17 -0400

    lab machines are finally running

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "libvirt" {
-  uri = "qemu:///session"
+  uri = "qemu:///system"
 }
 
 resource "libvirt_pool" "vm_storage" {
@@ -17,18 +17,29 @@ resource "libvirt_pool" "vm_storage" {
   path = "/var/lib/libvirt/images/rhcsa-lab"
 }
 
-resource "libvirt_network" "lab_network" {
-  name      = "rhcsa_network"
-  mode      = "nat"
-  addresses = ["192.168.100.0/24"]
-  autostart = true
+# resource "libvirt_network" "lab_network" {
+#   name        = "default"
+#   mode        = "nat"
+#   addresses   = ["192.168.124.0/24"]
+#   autostart   = true
+#
+#   dns {
+#     enabled = true
+#   }
+# }
+
+resource "libvirt_volume" "base_image" {
+  name   = "rhel-base.qcow2"
+  pool   = libvirt_pool.vm_storage.name
+  source = var.base_image
+  format = "qcow2"
 }
 
 resource "libvirt_volume" "rhel_disk" {
   count  = var.vm_count
   name   = "${var.node_prefix}-${count.index + 1}.qcow2"
   pool   = libvirt_pool.vm_storage.name
-  source = var.base_image
+  base_volume_id = libvirt_volume.base_image.id 
   format = "qcow2"
 }
 
@@ -40,21 +51,39 @@ resource "libvirt_cloudinit_disk" "cloud_init" {
     hostname      = "${var.node_prefix}-${count.index + 1}"
     user_password = "redhat"
   })
+  meta_data = templatefile("${path.module}/cloud-init/meta-data", {
+    instance_id = "${var.node_prefix}-${count.index + 1}"
+    hostname    = "${var.node_prefix}-${count.index + 1}"
+  })
 }
 
 resource "libvirt_domain" "rhel_node" {
   count  = var.vm_count
   name   = "${var.node_prefix}-${count.index + 1}"
+
+  # machine = "q35"
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
   memory = var.vm_memory
   vcpu   = var.vm_cpu
 
   disk {
     volume_id = libvirt_volume.rhel_disk[count.index].id
+    scsi      = false
   }
 
   network_interface {
-    network_id     = libvirt_network.lab_network.id 
-    wait_for_lease = true
+    network_name     = "default"
+    # wait_for_lease = true
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
   }
 
   cloudinit = libvirt_cloudinit_disk.cloud_init[count.index].id
